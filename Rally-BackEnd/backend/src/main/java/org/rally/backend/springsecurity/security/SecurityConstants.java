ffd2365494e247e02c424ecbc57777ee3e563531
AJmints <alexjmints@gmail.com>
2023-09-28 15:21:50 -0500

    update .env notes

--- Rally-BackEnd/backend/src/main/java/org/rally/backend/springsecurity/security/SecurityConstants.java
+++ Rally-BackEnd/backend/src/main/java/org/rally/backend/springsecurity/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package org.rally.backend.springsecurity.security;
 public class SecurityConstants {
 
     /** Security constants for decoding and verifiying JWT tokens **/
-    /** These are here for this project to run, but need to use a different approach with real project **/
+    /** These are here as an example. In a real project, we would use a .env file **/
     public static final long JWT_EXPIRATION = 21600000; // 6 hours = 21600000milliseconds
     public static final String JWT_SECRET = "LoremIpsumDolorSitAmetConsecteturAdipiscingElit";
 }
